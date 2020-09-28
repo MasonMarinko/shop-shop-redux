61441ba36218548b1d82fbf7fcd7fdfd23c62237
MasonM <Mason.P.Marinko@gmail.com>
2020-09-28 13:49:25 -0600

    fixed bugs with helper and cart, error when adding to cart, seeding wasn't deploying on site

---
+++ client/build/precache-manifest.a455ec64196d004827b517a8cbb6450a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db9709c7e3d3edd024a270291afe1cba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61c00fb350e73872eb3b",
+    "url": "/static/css/main.49b21ed8.chunk.css"
+  },
+  {
+    "revision": "ad979ec37b8cd104cf3c",
+    "url": "/static/js/2.e1cb8acc.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.e1cb8acc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61c00fb350e73872eb3b",
+    "url": "/static/js/main.6de170ea.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -1,71 +1,70 @@
-import React from 'react';
+import React from "react";
 // import { useStoreContext } from '../../utils/GlobalState';
-import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
+import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
 import { idbPromise } from "../../utils/helpers";
-import { useDispatch } from 'react-redux';
-
+import { useDispatch } from "react-redux";
 
 const CartItem = ({ item }) => {
-    const dispatch = useDispatch();
+  // const [, dispatch] = useStoreContext();
+  const dispatch = useDispatch();
 
-    const removeFromCart = item => {
-        dispatch({
-          type: REMOVE_FROM_CART,
-          _id: item._id
-        });
-        idbPromise('cart', 'delete', { ...item });
-      };
+  //function that dispatches to an action to remove an item from the cart and then update the state of the cart
+  const removeFromCart = (item) => {
+    dispatch({
+      type: REMOVE_FROM_CART,
+      _id: item._id,
+    });
+    idbPromise("cart", "delete", { ...item });
+  };
 
-    const onChange = (e) => {
-        const value = e.target.value;
+  //function to that dispatch to an action to update the quantity of an item in a cart. If the state of the item in the cart is 0, it will remove it completely.
+  const onChange = (e) => {
+    const value = e.target.value;
 
-        if (value === '0') {
-            dispatch({
-              type: REMOVE_FROM_CART,
-              _id: item._id
-            });
-          
-            idbPromise('cart', 'delete', { ...item });
-          } else {
-            dispatch({
-              type: UPDATE_CART_QUANTITY,
-              _id: item._id,
-              purchaseQuantity: parseInt(value)
-            });
-          
-            idbPromise('cart', 'put', { ...item, purchaseQuantity: parseInt(value) });
-          }
-    };
+    if (value === "0") {
+      dispatch({
+        type: REMOVE_FROM_CART,
+        _id: item._id,
+      });
+      idbPromise("cart", "delete", { ...item });
+    } else {
+      dispatch({
+        type: UPDATE_CART_QUANTITY,
+        _id: item._id,
+        purchaseQuantity: parseInt(value),
+      });
+      idbPromise("cart", "put", { ...item, purchaseQuantity: parseInt(value) });
+    }
+  };
 
-    return (
-        <div className="flex-row">
-            <div>
-                <img
-                    src={`/images/${item.image}`}
-                    alt=""
-                />
-            </div>
-            <div>
-                <div>{item.name}, ${item.price}</div>
-                <div>
-                    <span>Qty:</span>
-                    <input
-                        type="number"
-                        placeholder="1"
-                        value={item.purchaseQuantity}
-                        onChange={onChange}
-                    />
-                    <span
-                        role="img"
-                        aria-label="trash"
-                        onClick={() => removeFromCart(item)}
-                    >
-                        🗑️
-                    </span>
-                </div>
-            </div>
+  return (
+    <div className="flex-row">
+      <div>
+        <img src={`/images/${item.image}`} alt="" />
+      </div>
+      <div>
+        <div>
+          {item.name}, ${item.price}
         </div>
-    );
-}
+        <div>
+          <span>Qty:</span>
+          <input
+            type="number"
+            placeholder="1"
+            value={item.purchaseQuantity}
+            onChange={onChange}
+          />
+          <span
+            role="img"
+            aria-label="trash"
+            onClick={() => removeFromCart(item)}
+          >
+            🗑️
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default CartItem;
